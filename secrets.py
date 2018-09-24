34b7e83324760a6451c25dd80e4a6287efdac014
G3bE <32980656+G3bE@users.noreply.github.com>
2018-09-24 14:46:02 +0200

    Extra file for token, prefix and owner id

---
+++ secrets.py
@@ -0,0 +1,4 @@
+TOKEN = 'YOUR_TOKEN_HERE'
+MyID = 123456789098765432
+# Your ID there ^ : https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-
+PREFIX = "YOUR_PREFIX_HERE"
